02aa4cf6b40ab4f92b4f1533cd856c7137870c41
Naiguel Santos <naiguel.santos@objective.com.br>
2021-09-15 16:04:16 -0300

    remove comments

--- test/cool_program_test.rb
+++ test/cool_program_test.rb
@@ -3,10 +3,8 @@
 require 'minitest/autorun'
 require_relative '../lib/cool_program'
 
-# Test program coolness
 class CoolProgramTest < Minitest::Test
   def test_coolness_off_the_charts
-    # skip
     assert_equal CoolProgram.new.coolness, 11
   end
 end
